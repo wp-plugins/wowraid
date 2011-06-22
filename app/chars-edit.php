cfaa6c5304195f96d6c924a233a506beb70597a1
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-22 18:47:19 +0000

    deleted not longer in development!!
    git-svn-id: https://plugins.svn.wordpress.org/wowraid/trunk@399964 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app/chars-edit.php
+++
@@ -1,8 +0,0 @@
-	<tr class="form-field">
-		<th scope="row"><label for="role"><?php _e('User'); ?></label></th>
-		<td>
-			<?php
-			wp_dropdown_users(array('selected'=>2,'name'=>'userID'));
-			?>
-		</td>
-	</tr>
